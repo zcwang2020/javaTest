b7bb23077f9df7550b12a72c048e8ea773a43f54
zcwang2020 <zcwang2016@126.com>
2022-09-04 20:07:48 +0800

    add: mybatis学习

---
+++ mybatis-generate/src/test/java/TestMBG.java
@@ -0,0 +1,24 @@
+import com.study.mybatis.mapper.EmpMapper;
+import com.study.mybatis.pojo.Emp;
+import com.study.mybatis.pojo.EmpExample;
+import com.test.utils.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @Author wzc
+ * @Date 2022/9/3
+ */
+public class TestMBG {
+    @Test
+    public void testMBG() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        EmpExample empExample = new EmpExample();
+        empExample.createCriteria().andEmpNameEqualTo("张三");
+        List<Emp> emps = mapper.selectByExample(empExample);
+        System.out.println("emps = " + emps);
+    }
+}
