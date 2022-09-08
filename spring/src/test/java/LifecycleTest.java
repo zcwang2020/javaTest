364a431e0705ed6d29e4cadccf071ccef5f75961
“zcwang” <wb.wangzhichao01@mesg.corp.netease.com>
2022-09-08 17:17:50 +0800

    add: bean的生命周期

--- spring/src/test/java/LifecycleTest.java
+++ spring/src/test/java/LifecycleTest.java
@@ -20,8 +20,10 @@ public class LifecycleTest {
     /**
      * 1.实例化
      * 2.依赖注入
-     * 3.初始化
-     * 4.销毁
+     * 3.后置处理器的postProcessBeforeInitialization方法
+     * 4.初始化，需要通过bean的init-method属性指定初始化的方法
+     * 5.后置处理器的postProcessAfterInitialization方法
+     * 6.IOC容器关闭时销毁,需要通过bean的destroy-method属性指定销毁的方法
      */
 
     @Test
