5848ed1a543912c161d565e656e56ea02844b4b7
“zcwang” <wb.wangzhichao01@mesg.corp.netease.com>
2022-10-13 10:44:20 +0800

    add: springboot监控

---
+++ springboot26_admin_client/src/test/java/com/test/Springboot26AdminClientApplicationTests.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot26AdminClientApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
