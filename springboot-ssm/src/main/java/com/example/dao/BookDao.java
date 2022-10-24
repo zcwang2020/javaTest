8b8eed6b10222a80b04aa10e97ddbc3aa6c9437b
“zcwang” <wb.wangzhichao01@mesg.corp.netease.com>
2022-10-24 10:44:53 +0800

    add: springboot整合ssm

---
+++ springboot-ssm/src/main/java/com/example/dao/BookDao.java
@@ -0,0 +1,29 @@
+package com.example.dao;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+import com.example.domain.Book;
+
+public interface BookDao {
+
+//    @Insert("insert into tbl_book values(null,#{type},#{name},#{description})")
+    @Insert("insert into tbl_book (type,name,description) values(#{type},#{name},#{description})")
+    public void save(Book book);
+
+    @Update("update tbl_book set type = #{type}, name = #{name}, description = #{description} where id = #{id}")
+    public void update(Book book);
+
+    @Delete("delete from tbl_book where id = #{id}")
+    public void delete(Integer id);
+
+    @Select("select * from tbl_book where id = #{id}")
+    public Book getById(Integer id);
+
+    @Select("select * from tbl_book")
+    public List<Book> getAll();
+}
