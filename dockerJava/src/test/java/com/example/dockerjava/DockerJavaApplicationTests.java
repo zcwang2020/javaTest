4fb7249304b0058b223b2e1ee90671cc626f4439
“zcwang” <wb.wangzhichao01@mesg.corp.netease.com>
2023-03-03 14:25:02 +0800

    add: docker部署

---
+++ dockerJava/src/test/java/com/example/dockerjava/DockerJavaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.dockerjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJavaApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
