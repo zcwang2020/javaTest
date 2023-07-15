d24f9c5e2a0502a32ef2b122245a623a332b05d6
zcwang2020 <zcwang2016@126.com>
2023-07-15 15:05:56 +0800

    init: check push

--- io/src/main/java/io/IoTest.java
+++ io/src/main/java/io/IoTest.java
@@ -5,7 +5,9 @@ package io; /**
  * NETEASE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
 
+import com.alibaba.fastjson.JSON;
 import org.junit.Test;
+import work.ViewableUnitInfoVO;
 
 import java.io.File;
 import java.io.FileReader;
@@ -18,6 +20,14 @@ import java.io.IOException;
  * @Date 2021/10/26
  */
 public class IoTest {
+
+    public static void main(String[] args) {
+        ViewableUnitInfoVO viewableUnitInfoVO = new ViewableUnitInfoVO();
+        viewableUnitInfoVO.setDepartmentId(1L);
+        viewableUnitInfoVO.setAssignPoliceType(0);
+        String queryJson = JSON.toJSONString(viewableUnitInfoVO);
+        System.out.println(queryJson);
+    }
 
     @Test
     public void copy() {
---
+++ io/src/main/java/work/ViewableUnitInfoVO.java
@@ -0,0 +1,21 @@
+package work;
+
+import lombok.Data;
+
+/**
+ * @Author wzc
+ * @Date 2023/5/15
+ */
+@Data
+public class ViewableUnitInfoVO {
+
+    /**
+     * 部门id
+     */
+    private Long departmentId;
+
+    /**
+     * 警种id
+     */
+    private Integer assignPoliceType;
+}
